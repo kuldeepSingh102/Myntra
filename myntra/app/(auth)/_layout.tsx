e8c67845fddf6e86ba4c0fc76f0d7ccdcf21735b
Kuldeep Singh <ks2sept2005ks@gmail.com>
2026-06-27 17:38:35 +0530

    Add frontend back as normal folder

---
+++ myntra/app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#fff" },
+        headerTintColor: "#333",
+        headerTitleStyle: { fontWeight: "bold" },
+      }}
+    />
+  );
+}
